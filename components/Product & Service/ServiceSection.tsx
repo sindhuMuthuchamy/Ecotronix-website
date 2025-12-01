02b430a81f1bef02f067e068343c65b10850f727
sindhuMuthuchamy <sindhumuthuchamy@gmail.com>
2025-12-01 13:21:08 +0530

    product & service section + footer section finished

--- components/Product & Service/ServiceSection.tsx
+++ components/Product & Service/ServiceSection.tsx
@@ -4,26 +4,36 @@ import { useState } from "react";
 import { PRODUCTS } from "@/lib/constants";
 import ServiceList from "./ServiceList";
 import ServiceOrbit from "./ServiceOrbit";
+import ServiceOrbitMQ from "./SeriviceOrbitMQ";
+import './ServiceSection.css' 
 
 const ServiceSection = () => {
   const [activeIndex, setActiveIndex] = useState(0);
 
   return (
-    <section className="relative overflow-hidden bg-white py-20 lg:py-32 min-h-[800px]">
-      <div className="container mx-auto px-6 lg:px-12 relative z-10 grid grid-cols-1 xl:grid-cols-2 gap-12">
+    <section className="relative overflow-hidden bg-white py-20 lg:py-32 min-h-[800px]"  id="product-container">
+      <div className="container mx-auto px-6 lg:px-12 relative z-10 grid grid-cols-1 xl:grid-cols-2 gap-12" id="product-sec">
         
         {/* LEFT COLUMN: Text & List */}
+        <section className="hidden md:hidden lg:hidden xl:block">
         <ServiceList 
           products={PRODUCTS} 
           activeIndex={activeIndex} 
           setActiveIndex={setActiveIndex} 
         />
+        </section>
 
         {/* RIGHT COLUMN: Orbit Animation */}
         <ServiceOrbit 
           products={PRODUCTS} 
           activeIndex={activeIndex} 
-        />
+        /> 
+<section className=" xl:hidden lg:block md:block block">
+<ServiceOrbitMQ ></ServiceOrbitMQ>
+</section>
+        
+        
+        
       </div>
     </section>
     
